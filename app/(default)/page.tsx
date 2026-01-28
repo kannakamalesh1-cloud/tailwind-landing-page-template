9271893f8c3b6818721ce742fa6790b75c7367d1
kannakamalesh1-cloud <kannakamalesh1@gmail.com>
2026-01-28 18:31:27 +0530

    Update page.tsx

--- app/(default)/page.tsx
+++ app/(default)/page.tsx
@@ -1,7 +1,14 @@
+import Hero from "@/components/hero-home";
+import BusinessCategories from "@/components/business-categories";
+import FeaturesPlanet from "@/components/features-planet";
+import LargeTestimonial from "@/components/large-testimonial";
+import Cta from "@/components/cta";
+
 export const metadata = {
   title: "Home - Simple",
   description: "Page description",
 };
+
 export default function Home() {
   return (
     <>
